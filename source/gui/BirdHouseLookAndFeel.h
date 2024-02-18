64badad7767c9e19b9b288af52d7dc5474d19988
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2024-02-18 20:56:40 +0100

    fix: Small adjustments to look of combobox and mutebutton

--- source/gui/BirdHouseLookAndFeel.h
+++ source/gui/BirdHouseLookAndFeel.h
@@ -31,8 +31,12 @@ namespace BirdHouse
             setColour (juce::Slider::thumbColourId, Colours::blue);
             setColour (juce::Slider::trackColourId, Colours::fg);
             setColour (juce::Label::textColourId, Colours::fg);
-            setColour (juce::TextButton::buttonColourId, Colours::bg);
+
+            // setColour (juce::TextButton::buttonColourId, Colours::bgDark);
+            setColour (juce::TextButton::buttonColourId, Colours::bgDark);
+            setColour (juce::TextButton::buttonOnColourId, Colours::yellow);
             setColour (juce::TextButton::textColourOnId, Colours::fg);
+
             setColour (juce::ComboBox::backgroundColourId, Colours::bg);
             setColour (juce::ComboBox::textColourId, Colours::fg);
             setColour (juce::ComboBox::outlineColourId, Colours::fg.withAlpha (0.5f));
@@ -65,6 +69,66 @@ namespace BirdHouse
         juce::Font getTextButtonFont (juce::TextButton&, int buttonHeight) override
         {
             return getCustomFont().withHeight (buttonHeight / 2);
+        }
+
+        void drawButtonBackground (juce::Graphics& g, juce::Button& button, const juce::Colour& backgroundColour, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override
+        {
+            auto cornerSize = 0.0f;
+            auto lineThickness = 1.0f;
+            auto bounds = button.getLocalBounds().toFloat().reduced (0.5f, 0.5f);
+
+            auto baseColour = backgroundColour.withMultipliedSaturation (button.hasKeyboardFocus (true) ? 1.3f : 0.9f)
+                                  .withMultipliedAlpha (button.isEnabled() ? 1.0f : 0.5f);
+
+            if (shouldDrawButtonAsDown || shouldDrawButtonAsHighlighted)
+                baseColour = baseColour.contrasting (shouldDrawButtonAsDown ? 0.2f : 0.05f);
+
+            g.setColour (baseColour);
+
+            auto flatOnLeft = button.isConnectedOnLeft();
+            auto flatOnRight = button.isConnectedOnRight();
+            auto flatOnTop = button.isConnectedOnTop();
+            auto flatOnBottom = button.isConnectedOnBottom();
+
+            if (flatOnLeft || flatOnRight || flatOnTop || flatOnBottom)
+            {
+                juce::Path path;
+                path.addRoundedRectangle (bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight(), cornerSize, cornerSize, !(flatOnLeft || flatOnTop), !(flatOnRight || flatOnTop), !(flatOnLeft || flatOnBottom), !(flatOnRight || flatOnBottom));
+
+                g.fillPath (path);
+
+                g.setColour (button.findColour (juce::ComboBox::outlineColourId));
+                g.strokePath (path, juce::PathStrokeType (1.0f));
+            }
+            else
+            {
+                g.fillRoundedRectangle (bounds, cornerSize);
+
+                g.setColour (button.findColour (juce::ComboBox::outlineColourId));
+                g.drawRoundedRectangle (bounds, cornerSize, lineThickness);
+            }
+        }
+
+        void drawComboBox (juce::Graphics& g, int width, int height, bool, int, int, int, int, juce::ComboBox& box) override
+        {
+            // auto cornerSize = box.findParentComponentOfClass<juce::ChoicePropertyComponent>() != nullptr ? 0.0f : 3.0f;
+            auto cornerSize = 0.f;
+            juce::Rectangle<int> boxBounds (0, 0, width, height);
+
+            g.setColour (box.findColour (juce::ComboBox::backgroundColourId));
+            g.fillRoundedRectangle (boxBounds.toFloat(), cornerSize);
+
+            g.setColour (box.findColour (juce::ComboBox::outlineColourId));
+            g.drawRoundedRectangle (boxBounds.toFloat().reduced (0.5f, 0.5f), cornerSize, 1.0f);
+
+            juce::Rectangle<int> arrowZone (width - 30, 0, 20, height);
+            juce::Path path;
+            path.startNewSubPath ((float) arrowZone.getX() + 3.0f, (float) arrowZone.getCentreY() - 2.0f);
+            path.lineTo ((float) arrowZone.getCentreX(), (float) arrowZone.getCentreY() + 3.0f);
+            path.lineTo ((float) arrowZone.getRight() - 3.0f, (float) arrowZone.getCentreY() - 2.0f);
+
+            g.setColour (box.findColour (juce::ComboBox::arrowColourId).withAlpha ((box.isEnabled() ? 0.9f : 0.2f)));
+            g.strokePath (path, juce::PathStrokeType (2.0f));
         }
 
     private:
